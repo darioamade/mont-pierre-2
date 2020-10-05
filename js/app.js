00c1dc8a2489c71062506f72400df6a86973c521
darioamade <dario_isd@outlook.com>
2020-10-05 20:29:08 +0100

    added new files

--- js/app.js
+++ js/app.js
@@ -1,11 +1,62 @@
+// const menuOpen = document.querySelector('.menu-open');
+// const menuClose = document.querySelector('.menu-close');
+// const nav = document.querySelector('.navX');
 
-const menuOpen = document.querySelector('.menu-open');
-const menuClose = document.querySelector('.menu-close');
-const nav = document.querySelector('.navX');
+// menuOpen.addEventListener('click', menuTog);
+// menuClose.addEventListener('click', menuTog);
 
-menuOpen.addEventListener('click', menuTog);
-menuClose.addEventListener('click', menuTog);
+// function menuTog(e) {
+//     nav.classList.toggle('active')
+// };
 
-function menuTog(e){
-  nav.classList.toggle('active')
-};
+
+let checkAv = document.getElementById('checkbox');
+checkAv.addEventListener('click', () => {
+    alert('Page in development 🧑‍💻 ')
+})
+
+
+// (function(){
+//     'use strict';
+
+//     let newElement = document.createElement('div');
+//     newElement.textContent = "I am a new Element";
+
+//     let list = document.querySelector('#myList')
+//     // let list = document.getElementsById('myList');
+//     //  list.appendChild(newElement);
+//      list.insertBefore(newElement, list.firstElementChild);
+
+//      let replaceElement = document.createElement('li');
+//      replaceElement.textContent = 'I am replacing you'
+
+//     list.replaceChild(replaceElement, newElement);
+
+
+//     console.log(newElement)
+
+// }());
+
+//
+
+// let newLi = document.createElement('li');
+
+// let newA = document.createElement('a');
+
+
+// let menu = document.getElementById('myList').getElementsByClassName('menu-open')[0];
+// menu.appendChild(newLi);
+
+// newLi.appendChild(newA);
+
+// newA.innerHTML ='Blog';
+
+
+// const elem = document.createElement('h1');
+// // console.log(elem);
+
+// const elemText = document.createTextNode('This is a Div');
+
+
+// elem.appendChild(elemText);
+// document.body.appendChild(elem);
--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -1,13 +1,38 @@
 "use strict";
 
-var menuOpen = document.querySelector('.menu-open');
-var menuClose = document.querySelector('.menu-close');
-var nav = document.querySelector('.navX');
-menuOpen.addEventListener('click', menuTog);
-menuClose.addEventListener('click', menuTog);
-
-function menuTog(e) {
-  nav.classList.toggle('active');
-}
-
-;
+// const menuOpen = document.querySelector('.menu-open');
+// const menuClose = document.querySelector('.menu-close');
+// const nav = document.querySelector('.navX');
+// menuOpen.addEventListener('click', menuTog);
+// menuClose.addEventListener('click', menuTog);
+// function menuTog(e) {
+//     nav.classList.toggle('active')
+// };
+var checkAv = document.getElementById('checkbox');
+checkAv.addEventListener('click', function () {
+  alert('Page in development 🧑‍💻 ');
+}); // (function(){
+//     'use strict';
+//     let newElement = document.createElement('div');
+//     newElement.textContent = "I am a new Element";
+//     let list = document.querySelector('#myList')
+//     // let list = document.getElementsById('myList');
+//     //  list.appendChild(newElement);
+//      list.insertBefore(newElement, list.firstElementChild);
+//      let replaceElement = document.createElement('li');
+//      replaceElement.textContent = 'I am replacing you'
+//     list.replaceChild(replaceElement, newElement);
+//     console.log(newElement)
+// }());
+//
+// let newLi = document.createElement('li');
+// let newA = document.createElement('a');
+// let menu = document.getElementById('myList').getElementsByClassName('menu-open')[0];
+// menu.appendChild(newLi);
+// newLi.appendChild(newA);
+// newA.innerHTML ='Blog';
+// const elem = document.createElement('h1');
+// // console.log(elem);
+// const elemText = document.createTextNode('This is a Div');
+// elem.appendChild(elemText);
+// document.body.appendChild(elem);
